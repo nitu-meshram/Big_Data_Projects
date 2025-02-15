161f8222c6ee3224fe89a09d4da249c9da305ab7
Nitu_Meshram <nitumeshram10@gmail.com>
2025-02-15 20:57:19 +0530

    This is my first Program

---
+++ src/main/scala/First.scala
@@ -0,0 +1,15 @@
+
+object FIRST {
+
+  def main(args:Array[String]):Unit={
+
+    print("hello world")
+  }
+
+}
+object second {
+  def main(args: Array[String]): Unit = {
+    println("Scala and SBT are working perfectly fine!")
+  }
+}
+
