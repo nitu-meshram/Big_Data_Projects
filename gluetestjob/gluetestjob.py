3db1cf52305bc9a11d4517a5fe0049ac88a549e4
nitu-meshram <nitu.meshram14@gmail.com>
2025-04-28 01:05:25 +0530

    Synchronizing job gluetestjob from AWS Glue at 2025/04/27 19:35:25

---
+++ gluetestjob/gluetestjob.py
@@ -0,0 +1,18 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+df=spark.read.format("csv").option("header","True").option("path","s3://gluebucketapril27/details.csv").load()
+df.write.format("csv").option("header","True").option("path","s3://gluebucketapril27/output1").save()
+job.commit()
